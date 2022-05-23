12ec8bc4055730c1574edc95c271e0446e22db46
Randarola <67556224+Randarola@users.noreply.github.com>
2022-05-23 21:24:35 +0200

    dodanie timer'a

--- SimpleGames/MainWindow.xaml.cs
+++ SimpleGames/MainWindow.xaml.cs
@@ -18,12 +18,26 @@ namespace SimpleGames
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
+    using System.Windows.Threading;
     public partial class MainWindow : Window
     {
+        //utworzenie zegara
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void SetUpGame()
